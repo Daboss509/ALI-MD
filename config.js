dad4cc94b17ec891e4d3da9a638f2b99e7f4b9b1
Daboss509 <elijahpernellpierrecanel@gmail.com>
2025-05-05 20:16:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NbEBFazI#iJ2k71gxHOp2pjfDfSUdydrHVR7Jq3M3dKrXwgk5_vs",
+SESSION_ID: process.env.SESSION_ID || "cWMymJwK#1c7lxC5JpQ5YegM0wgIP2D9xdGC7p_72j-BtAAzhSCw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
